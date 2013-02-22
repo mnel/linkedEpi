6be452551efd18a3bd2a05be2c62a2345eeee505
Mark Greenaway <certifiedwaif@gmail.com>
2013-02-22 15:29:13 +1100

    Add merge.unique, with check.unique argument

--- R/checks.R
+++ R/checks.R
@@ -17,3 +17,26 @@
 check.unique <- function(data, id = 'ppn'){
   !any(duplicated(data[[id]]))
 }
+
+#' Merge data sets. Check that each data set being merged has a unique
+#' key
+#'
+#' @param x, y data frames, or objects to be coerced to one
+#' @param check.unique logical; if TRUE, x and y are checked to see that
+#' the column used for merging is unique
+#' @return Merged data set
+#' @export
+#' @examples
+#' df.non <- data.frame(ppn = c(1L,1L,2L), var1 = seq_len(3))
+#' df.unique <- data.frame(ppn = seq_len(3), var2 = seq_len(3))
+#' merge.unique(df.unique, df.non, by = 'ppn')
+#' # Error: check.unique(y, id = by) is not TRUE
+merge.unique <- function(..., check.unique=TRUE)
+{
+  args = list(...)
+  x = args[[1]]
+  y = args[[2]]
+  by = args[["by"]]
+  stopifnot(check.unique(x, id=by), check.unique(y, id=by))
+  return(do.call(merge, args))
+}
