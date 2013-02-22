5676c5810e95b535f780889651b8b3e0cd618f05
Mark Greenaway <certifiedwaif@gmail.com>
2013-02-22 15:30:09 +1100

    Add tests for merge.unique

--- inst/tests/check-tests.r
+++ inst/tests/check-tests.r
@@ -1,8 +1,5 @@
 context('Unique ppn identifer checks for ease of merging')
 
-
-
-
 test_that('check.unique returns FALSE for easy test case',{
   df.non <- data.frame(ppn = c(1L,1L,2L), var1 = seq_len(3))
   df.unique <- data.frame(ppn = seq_len(3), var2 = seq_len(3))
@@ -10,3 +7,8 @@ test_that('check.unique returns FALSE for easy test case',{
   expect_that(check.unique(df.unique), is_true())
 })
 
+test_that('merge.unique does not merge data sets with non-unique keys for easy test case',{
+  df.non <- data.frame(ppn = c(1L,1L,2L), var1 = seq_len(3))
+  df.unique <- data.frame(ppn = seq_len(3), var2 = seq_len(3))
+  expect_error(merge.unique(df.non, df.unique), "Error: check.unique(.*, id = by) is not TRUE")
+})
