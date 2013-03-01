db8a2a5dca12207707dd1b1e7f3b21707b410ab4
Mark Greenaway <certifiedwaif@gmail.com>
2013-03-01 22:38:52 +1100

    Test for merge.unique now runs and passes

--- inst/tests/check-tests.r
+++ inst/tests/check-tests.r
@@ -1,3 +1,5 @@
+library(testthat)
+
 context('Unique ppn identifer checks for ease of merging')
 
 test_that('check.unique returns FALSE for easy test case',{
@@ -10,5 +12,5 @@ test_that('check.unique returns FALSE for easy test case',{
 test_that('merge.unique does not merge data sets with non-unique keys for easy test case',{
   df.non <- data.frame(ppn = c(1L,1L,2L), var1 = seq_len(3))
   df.unique <- data.frame(ppn = seq_len(3), var2 = seq_len(3))
-  expect_error(merge.unique(df.non, df.unique), "Error: check.unique(.*, id = by) is not TRUE")
+  expect_error(merge.unique(df.non, df.unique, by="ppn"))
 })
